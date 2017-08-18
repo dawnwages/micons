d4d91a41709df06e2e0f1f6fd827d3fcbd385c8e
Dawn Wages <dawn.wages@gmail.com>
2017-08-18 18:32:23 +0000

    I'm trying to mess with the environment variables in order to deploy on PythonAnywhere. That didn't work so now I'm trying to deploy to Heroku, but I might as well stop and fix the actual site before I worry about deploying

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "micons.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "micons.settings.dev")
 
     from django.core.management import execute_from_command_line
 
--- micons/settings/base.py
+++ micons/settings/base.py
@@ -12,12 +12,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 from __future__ import absolute_import, unicode_literals
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+SECRET_KEY = 'mysecret'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
