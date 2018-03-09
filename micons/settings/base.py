98754434ff668e282d08831f79025a4af32bcf8b
Dawn Wages <dawn.wages@gmail.com>
2018-03-09 02:19:42 +0000

    heroku

--- micons/settings/base.py
+++ micons/settings/base.py
@@ -161,8 +161,8 @@ WAGTAIL_SITE_NAME = "micons"
 BASE_URL = 'http://example.com'
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
