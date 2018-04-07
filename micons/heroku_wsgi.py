ac5532c769adc622f3aad7fc6cb17dd39fbf8344
Dawn Wages <dawn.wages@gmail.com>
2018-04-07 02:43:47 -0400

    wagtail deployment settingss

---
+++ micons/heroku_wsgi.py
@@ -0,0 +1,7 @@
+
+from whitenoise.django import DjangoWhiteNoise
+
+from .wsgi import application as _application
+
+
+application = DjangoWhiteNoise(_application)
--- micons/settings/dev.py
+++ micons/settings/dev.py
@@ -6,12 +6,11 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+51mvjypkj(6pt^d$2+e_rry(3^yh5k0fqj42gmcvxw)9_^rzo'
-mysecret = '+2p^yh!0gj*7bd0k6xrr9x)tab+fyp#4eu9tf!6kefcp*n&22$'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+BASE_URL = 'http://localhost:8000'
 
 try:
     from .local import *
